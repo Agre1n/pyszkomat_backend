4cca471b9eba059a0787847c3cbb7a5a1ca86aed
Agrein <michalsokolowski12@gmail.com>
2024-03-16 23:53:27 +0100

    project scheme from Spring Initializer

---
+++ src/main/java/com/example/pyszkomat_backend/PyszkomatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pyszkomat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyszkomatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyszkomatBackendApplication.class, args);
+    }
+
+}
